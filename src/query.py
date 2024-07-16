c60f27e43b930893bfea2ff2f18e3dfa53831d6a
nguyenhoangthanhly <117055865+nguyenhoangthanhly@users.noreply.github.com>
2024-07-17 01:36:06 +0700

    Update query.py

--- src/query.py
+++ src/query.py
@@ -18,17 +18,3 @@ def view_all_data():
     c.execute('select * from adidas_4 order by id asc')
     data=c.fetchall()
     return data
-
-def update_data_to_mysql(df):
-    # Chèn dữ liệu vào bảng
-    for i, row in df.iterrows():
-        sql = """
-            INSERT INTO adidas_ (Retailer, Region, State, City, Product, `Price per Unit`, `Units Sold`, `Operating Profit`, `Sales Method`, `Total Sales`, Month, Year, Day, Season, Date)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """
-        c.execute(sql, tuple(row))
-
-    conn.commit()
-    cursor.close()
-    conn.close()
-    st.write("Dữ liệu đã được cập nhật lên MySQL")
